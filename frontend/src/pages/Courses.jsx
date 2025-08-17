0ca00aff37ac04a345536c96646d2e37fa110f79
rai-12300063 <racilarde7678@gmail.com>
2025-08-17 14:22:13 +1000

    OLPT636-45 Runtime Error Fixes

--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { useAuth } from '../context/AuthContext';
 import axiosInstance from '../axiosConfig';
 import { Link } from 'react-router-dom';
 
