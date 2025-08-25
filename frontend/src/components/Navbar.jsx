f46ef789aeda67739ac3fea5fa6bf2eb5ec628bd
rai-12300063 <racilarde7678@gmail.com>
2025-08-25 15:51:27 +1000

    OLPT636-69 Update Dashboard

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-blue-600 text-white p-4 flex justify-between items-center">
+    <nav className="bg-green-600 text-white p-4 flex justify-between items-center">
       <Link to="/" className="text-2xl font-bold">Learning Tracker</Link>
       <div>
         {user ? (
